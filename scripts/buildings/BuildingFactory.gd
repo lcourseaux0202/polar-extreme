224e88a50f7a04bf4e653f8019ed1d4b9009c9a3
evolve <elivanoa5@gmail.com>
2025-10-13 14:05:50 +0200

    comment out buildingfactory

--- scripts/buildings/BuildingFactory.gd
+++ scripts/buildings/BuildingFactory.gd
@@ -1,26 +1,26 @@
-class_name BuildingFactory
-
-signal building_mode
-signal building_select
-signal building_placement
-
-var toPlace :BaseBuilding
-
-func place(){
-	place(toPlace);
-}
-
-
-var available_buildings = []
-
-func createLabo(pos: Vector2):
-	var labo = Laboratory.new()
-	labo.constructs(pos)
-
-func createToilets(pos: Vector2):
-	var toilets = Toilets.new()
-	toilets.constructs(pos)
-
-func createIceMine(pos: Vector2):
-	var ice_mine = IceMine.new()
-	ice_mine.constructs(pos)
+#class_name BuildingFactory
+#
+#signal building_mode
+#signal building_select
+#signal building_placement
+#
+#var toPlace :BaseBuilding
+#
+#func place(){
+	#place(toPlace);
+#}
+#
+#
+#var available_buildings = []
+#
+#func createLabo(pos: Vector2):
+	#var labo = Laboratory.new()
+	#labo.constructs(pos)
+#
+#func createToilets(pos: Vector2):
+	#var toilets = Toilets.new()
+	#toilets.constructs(pos)
+#
+#func createIceMine(pos: Vector2):
+	#var ice_mine = IceMine.new()
+	#ice_mine.constructs(pos)
