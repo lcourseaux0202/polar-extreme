36e8d8abb0553b37176dd2123901b98b9f63b94f
Lucas Courseaux <lucasc33680@gmail.com>
2025-12-18 17:00:10 +0100

    Add settings button to main menu

--- scripts/UI/MainMenu.gd
+++ scripts/UI/MainMenu.gd
@@ -2,6 +2,7 @@ extends Control
 class_name MainMenu
 
 @onready var quit_game_btn: Button = $VBoxContainer/QuitGameBtn
+@onready var settings_btn: Button = $VBoxContainer/SettingsBtn
 @onready var start_game_btn: Button = $VBoxContainer/StartGameBtn
 @onready var tutorial = preload("res://scenes/UI/Tutorial.tscn")
 
@@ -14,6 +15,9 @@ func _ready() -> void:
 	quit_game_btn.mouse_entered.connect(_quit_button_enter)
 	quit_game_btn.mouse_exited.connect(_quit_button_exit)
 	quit_game_btn.pressed.connect(_quit_game)
+	settings_btn.mouse_entered.connect(_settings_button_enter)
+	settings_btn.mouse_exited.connect(_settings_button_exit)
+	settings_btn.pressed.connect(settingsValue.open)
 	call_deferred("init_pivot")
 	
 func init_pivot():
@@ -25,6 +29,12 @@ func _start_button_enter():
 	
 func _start_button_exit():
 	create_tween().tween_property(start_game_btn, "scale", Vector2.ONE, 0.1).set_trans(Tween.TRANS_SINE)
+
+func _settings_button_enter():
+	create_tween().tween_property(settings_btn, "scale", hover_scale, 0.1).set_trans(Tween.TRANS_SINE)
+	
+func _settings_button_exit():
+	create_tween().tween_property(settings_btn, "scale", Vector2.ONE, 0.1).set_trans(Tween.TRANS_SINE)
 
 func _quit_button_enter():
 	create_tween().tween_property(quit_game_btn, "scale", hover_scale, 0.1).set_trans(Tween.TRANS_SINE)
