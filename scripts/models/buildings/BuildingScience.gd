3e59fc18e59c8c31e2c38a59269dd1e21bc1c516
DAUSSE CAPUCINE <capucine.dausse@etu.unistra.fr>
2025-12-19 09:47:40 +0100

    Add new icons + change icons in buttons for closing menus

--- scripts/models/buildings/BuildingScience.gd
+++ scripts/models/buildings/BuildingScience.gd
@@ -123,11 +123,11 @@ func get_science_production_ratio() -> float:
 func get_infos() -> String:
 	var text = ""
 	if science_per_second > 0:
-		text += "Science produite chaque seconde : " + str(int(science_per_second)) + "\n"
+		text += "Science par seconde : " + str(int(science_per_second)) + " par scientifique\n"
 	if scientist_places > 0:
 		text += "Capacité de scientifiques : +" + str(int(scientist_places)) + "\n"
 	if pollution_per_second > 0:
-		text += "Pollution par seconde :" + str(int(pollution_per_second)) + "\n"
+		text += "Pollution par seconde : " + str(int(pollution_per_second)) + "\n"
 	if wellness_value > 0:
 		text += "Augmentation du bien être : " + str(int(wellness_value))
 	return text
